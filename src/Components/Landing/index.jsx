1c1b4a1638ab2749c025b0156988d676b8aadd24
dan9b6 <dkburton0@gmail.com>
2020-11-10 19:31:54 +0000

    reconstructed css for background image

--- src/Components/Landing/index.jsx
+++ src/Components/Landing/index.jsx
@@ -5,7 +5,6 @@ const Landing = () => {
   return (
     <div className="landing">
       <img className="mobile-img" src={require('../../Images/me-mobile.jpg')} alt="me" />
-      <img className="desktop-img" src={require('../../Images/me-desktop.jpg')} alt="me" />
       <div className="intro">
         <h1>Hello. I’m Dan. I’m an aspiring web developer looking for my first role.</h1>
         <h5>
