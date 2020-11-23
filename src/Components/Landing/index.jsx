84cbc4e640fbe2e51a9176db61c3ea33777440a3
dan9b6 <dkburton0@gmail.com>
2020-11-23 09:07:32 +0000

    amended Landing page

--- src/Components/Landing/index.jsx
+++ src/Components/Landing/index.jsx
@@ -6,7 +6,7 @@ const Landing = () => {
     <div className="landing">
       <img src={require('../../Images/me-mobile.jpg')} alt="me" />
       <div className="intro">
-        <h1>Hello. I’m Dan. I’m an aspiring web developer looking for my first role.</h1>
+        <h1>Hello. I’m Dan. I’m a Web Developer looking for a new role!</h1>
         <h5>
           Are you considering employing a Junior Web Developer? How long have you got to be
           convinced I’m your guy?
